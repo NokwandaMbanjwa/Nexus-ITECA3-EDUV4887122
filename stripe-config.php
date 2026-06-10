08cddbd01f8a480bd3e4429e38a7a64f427c4e30
NokwandaMbanjwa <nokwandambanjwa359@gmail.com>
2026-06-10 20:28:44 +0200

    Add files via upload

--- stripe-config.php
+++ stripe-config.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'stripe/init.php'; 
 
-\Stripe\Stripe::setApiKey('sk_test_51TaJeXCmUXFfLTAkAsPDaBcJfaHCcZvKzPvvaImgtrdIfjmJWFLyScMNlNnjHfGsJS26GBSaJ0pwMLLBMxTV7ZJ900rDarXv6S'); 
+\Stripe\Stripe::setApiKey(''); 
 ?>
